dbc9cda50f21610583875ea39276a5d240c84ad5
John Stevans <jstevans@users.noreply.github.com>
2022-11-14 20:16:39 -0800

    Update main.js

--- main.js
+++ main.js
@@ -33,9 +33,16 @@ const overrideManifestRedirect = (manifest) => ({
 })
 
 const initiate = ({organization, manifest, hideManifest}) => {
-  const manifestString = JSON.stringify(overrideManifestRedirect(manifest));
-  const appUrl = getAppUrl(organization);
-  document.body.appendChild(renderForm(appUrl, manifest, hideManifest));
+  try {
+		const manifestObj = !!manifest && manifest.length > 0 ? JSON.parse(manifest) : {};
+		const manifestString = JSON.stringify(overrideManifestRedirect(manifest));
+  	const appUrl = getAppUrl(organization);
+  	document.body.appendChild(renderForm(appUrl, manifestString, hideManifest));
+	} catch (e) {
+		const div = createElement("div");
+		div.innerText = `ERROR: ${e}`;
+		document.body.appendChild(div);
+	}
 }
 
 /* AFTER-REDIRECT FLOW */
